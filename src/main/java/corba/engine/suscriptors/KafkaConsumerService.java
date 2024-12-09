a6e8f79605c9f70a216bd283273dbec16715d7c6
sircheka <reibinsql@gmail.com>
2024-12-09 11:58:06 -0500

    se agrega consumer kafka

--- src/main/java/corba/engine/suscriptors/KafkaConsumerService.java
+++ src/main/java/corba/engine/suscriptors/KafkaConsumerService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class KafkaConsumerService {
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -22,25 +24,29 @@ public class KafkaConsumerService {
     @KafkaListener(topics = "__consumer_offsets", groupId = "mi-grupo-consumidor")
     public void listen(String message) {
         try {
-            KafkaData kafkaData = objectMapper.readValue(message, KafkaData.class);
-            processKafkaData(kafkaData);
+            List<KafkaData> kafkaDataList = objectMapper.readValue(message, objectMapper.getTypeFactory().constructCollectionType(List.class, KafkaData.class));
+            processKafkaData(kafkaDataList);
         } catch (Exception e) {
             System.err.println("Error procesando el mensaje: " + e.getMessage());
         }
     }
-    private void processKafkaData(KafkaData data) {
-        Tags tags = data.getTags();
-        if ("10.95.90.87".equals(tags.getSource())) {
-            System.out.println("Alerta: Los datos provienen de la fuente especificada (" + tags.getSource() + ").");
-        }
-        ruleService.executeRulesWithEventKafka(data);
+    private void processKafkaData(List<KafkaData> data) {
+        for (KafkaData dataEvaluar: data) {
+            Tags tags = dataEvaluar.getTags();
+            if ("10.95.90.87".equals(tags.getSource())) {
+                System.out.println("Alerta: Los datos provienen de la fuente especificada (" + tags.getSource() + ").");
+            }
+            ruleService.executeRulesWithEventKafka(dataEvaluar);
 
-        System.out.println("Datos transformados: ");
-        System.out.println("Nombre: " + data.getName());
-        System.out.println("Timestamp: " + data.getTimestamp());
-        System.out.println("Componente: " + tags.getComponentName());
-        System.out.println("Source: " + tags.getSource());
-        System.out.println("Subscription Name: " + tags.getSubscriptionName());
-        System.out.println("Values: " + data.getValues());
+            System.out.println("Datos transformados: ");
+            System.out.println("Nombre: " + dataEvaluar.getName());
+            System.out.println("Timestamp: " + dataEvaluar.getTimestamp());
+            System.out.println("Componente: " + tags.getComponentName());
+            System.out.println("Source: " + tags.getSource());
+            System.out.println("Subscription Name: " + tags.getSubscriptionName());
+            System.out.println("Values: " + dataEvaluar.getValues());
+        }
+
+
     }
 }
