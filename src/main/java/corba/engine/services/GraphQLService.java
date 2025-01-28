2b9296ca3cdade4ba42adb6256a98780693809d8
sircheka <reibinsql@gmail.com>
2025-01-28 17:23:30 -0500

    se agrega cambios para nueva api

--- src/main/java/corba/engine/services/GraphQLService.java
+++ src/main/java/corba/engine/services/GraphQLService.java
@@ -69,7 +69,7 @@ public class GraphQLService {
         System.out.println("Enviando solicitud GraphQL TT: " + requestBody);
 
         return webClient.post()
-                //.uri("/oc/graphql") // Cambia según el endpoint correcto
+                .uri("http://10.95.90.64:10000/oc/graphql") // Cambia según el endpoint correcto
                 .bodyValue(graphqlQuery)
                 .retrieve()
                 .onStatus(HttpStatus::isError, response -> response.bodyToMono(String.class)
